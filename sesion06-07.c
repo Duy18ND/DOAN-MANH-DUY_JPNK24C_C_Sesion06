ce362463d90ef1d645466804a56a568ae6ae9713
Duy18ND <duy18namdinh@gmail.com>
2024-11-27 00:17:04 +0700

    Add files via upload

---
+++ sesion06-07.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int uocchung,n;
+	printf("Nhap So Nguyen Bat Ky  ");
+	scanf("%d",&uocchung);
+	for(int i = 1; i <uocchung; i++){
+		if(i <= uocchung){
+			n = uocchung / i;
+			printf("%d \n",n);
+		}
+		}
+		}
+	return 0;
+
